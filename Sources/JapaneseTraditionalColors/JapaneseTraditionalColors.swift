47e9c445a8cb80d57ce092dae0881d189105f3ff
feb19 <nt@feb19.jp>
2021-05-06 10:25:00 +0900

    update

--- Sources/JapaneseTraditionalColors/JapaneseTraditionalColors.swift
+++ Sources/JapaneseTraditionalColors/JapaneseTraditionalColors.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 // swiftlint:disable type_body_length
 public class JapaneseTraditionalColors {
-    public static let ikkonzome = JapaneseTraditionalColor(rgb: [240, 143, 144], name: "一斤染", yomi: "Ikkonzome")
+    static let ikkonzome = JapaneseTraditionalColor(rgb: [240, 143, 144], name: "一斤染", yomi: "Ikkonzome")
     public var kobaiiro = JapaneseTraditionalColor(rgb: [219, 90, 107], name: "紅梅色", yomi: "Kōbai-iro")
     public var sakurairo = JapaneseTraditionalColor(rgb: [252, 201, 185], name: "桜色", yomi: "Sakura-iro")
     public var usubeni = JapaneseTraditionalColor(rgb: [242, 102, 108], name: "薄紅", yomi: "Usubeni")
