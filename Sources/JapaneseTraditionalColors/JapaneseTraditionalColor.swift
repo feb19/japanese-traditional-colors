c5e6718aa9264087e3e4912cd01931bf005b2e7d
feb19 <nt@feb19.jp>
2022-01-23 23:57:36 +0900

    コメントの追加

--- Sources/JapaneseTraditionalColors/JapaneseTraditionalColor.swift
+++ Sources/JapaneseTraditionalColors/JapaneseTraditionalColor.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 // swiftlint:disable identifier_name
+/// 日本の伝統色を表す構造体です。
 public struct JapaneseTraditionalColor: Identifiable {
+    /// 色ごとに区別するための識別子
     public var id: UUID
-    public let rgb: [Double]!
-    public let name: String!
-    public let yomi: String!
+    /// R,G,B の配列
+    public let rgb: [Double]
+    /// 英語での名前
+    public let name: String
+    /// 日本語での読み
+    public let yomi: String
     public init(rgb: [Double], name: String, yomi: String) {
         self.rgb = rgb
         self.name = name
